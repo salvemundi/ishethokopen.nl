d7b99cbdac15153b770956f19cc894a1ff473cbe
Quint <quint.legius@gmail.com>
2022-09-09 16:30:34 +0200

    created api with test middleware

--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use App\Models\Status;
+use Illuminate\Support\Str;
 
 class statusController extends Controller
 {
-    public function
+    public function store(Request $request): void {
+        $status = Status::first();
+        if($status == null) {
+            $status = new Status();
+            $status->id = Str::uuid()->toString();
+        }
+        $status->status = $request->status;
+        $status->save();
+    }
+
+    public function show(): Factory|View|Application
+    {
+        $status = Status::first()->status;
+        return view('welcome', ['hokStatus' => $status]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StatusController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::middleware('apiAuth')->post('/v1/hokStatus', [StatusController::class,'store']);
