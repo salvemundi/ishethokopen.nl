be79c4229b8910ee45aa5d41e2c9575e363611d4
Quint <quint.legius@gmail.com>
2022-09-19 12:43:25 +0200

    fix 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(config('app.env') === 'production') {
-            \URL::forceScheme('https');
+        If (env('APP_ENV') !== 'local') {
+            $this->app['request']->server->set('HTTPS', true);
         }
     }
 }
